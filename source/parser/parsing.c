b06000adc044e7e6d6618072b4b2417ed504d179
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-07 19:55:36 +0100

    add readline & double quotes traker + lexer

--- source/parser/parsing.c
+++ source/parser/parsing.c
@@ -1,9 +1,34 @@
 #include "minishell.h"
 
+static char *reader_loop(void)
+{
+    char *line;
+
+    while (true)
+    {
+        line = readline("minishell> ");
+        if (line == NULL || line[0] == 0 || ft_isspace(0, line))
+            continue;
+        return (line);
+    }
+    return (NULL);
+}
+
+static void preprocess(char *line)
+{
+    check_quotes(line);
+    check_pipe(line);
+    check_tokens(line);
+}
+
 void    parsing(char *env[])
 {
     t_all *core
 
     core = get_core();
     core->env = env;
+    core->in_line = reader_loop();
+    core->inline_len = ft_strlen(core->in_line);
+    preprocess(line);
+    load_elements(line);
 }
