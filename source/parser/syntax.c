ee1b33e00bf6735ec1dcc0cb86caf3e881d25ac1
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-07 19:56:12 +0100

    add double quotes tracker and function to load tokens

---
+++ source/parser/syntax.c
@@ -0,0 +1,45 @@
+#include "minishell.h"
+
+void    load_elements(char *line)
+{
+    long long i;
+
+    i = -1;
+    while (line[++i])
+    {
+        while (line[i] && ft_isspace(line[i], NULL))
+            i++;
+        if (ft_istoken(line[i]))
+            i += lexer_add(line + (i + 1), TOKEN);
+        else if (line[i] == D_QUOTES)
+            i += lexer_add(line + (i + 1), D_QUOTES);
+        else if (line[i] == S_QUOTES)
+            i += lexer_add(line + (i + 1), S_QUOTES);
+        else
+            i += lexer_add(line + (i + 1), WORD);
+    }
+}
+
+void    check_quotes(char *line)
+{
+    long long i;
+
+    i = -1;
+    while (line[++i])
+    {
+        if (line[i] == S_QUOTES)
+        {
+            while (line[i] && line[i] != S_QUOTES)
+                i++;
+            if (!line[i])
+                pexit(SQ_ERR, 1);
+        }
+        else if (line[i] == D_QUOTES)
+        {
+            while (line[i] && line[i] != D_QUOTES)
+                i++;
+            if (!line[i])
+                pexit(DQ_ERR, 1);
+        }
+    }
+}
