e4db740ad33aee8970464ab5ed78c2228ae2db26
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-07 19:50:26 +0100

    add lexer macros + err string macros

--- includes/macros.h
+++ includes/macros.h
@@ -7,21 +7,50 @@
 
 // macros for perror message
 # define PRGM_NAME "minishell"
-# define MEM_ERR "minishell: malloc"
+# define MEM_ERR ": malloc"
+# define DQ_ERR ": syntax error: unclosed token `\"'\n"
+# define SQ_ERR ": syntax error: unclosed token `''\n"
+# define PIPE_ERR ": syntax error: unclosed token `|'\n"
 
 
 /**
  * @brief macros for "clear" function
  * 
- * @see malloc_v2()
+ * @see galloc()
  */
 # define F_ALL 10
 # define F_TMP 11
 
+/**
+ * @brief macros for tokens and syntax
+ */
 
+// double quotes ascii
+# define D_QUOTES 34
 
+// single quotes ascii
+# define S_QUOTES 39 // single quotes ascii
 
+// heredoc
+# define HERE_DOC 0
 
+// infile redirection in overwrite mode
+# define IN_RDRT 1
+
+// outfile redirection in overwrite mode
+# define OUT_RDRT_OW 2
+
+// outfile redirection in appende mode
+# define OUT_RDRT_APP 3
+
+// word type token 
+# define WORD 4
+
+// pipe type token
+# define PIPE 5
+
+// any token type
+# define TOKEN 6
 
 
 
