8a323f70ee4eb30b28e136d04479ad7ffab3ee25
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-13 16:34:26 +0100

    fix funtion logic

--- source/utils/utils.c
+++ source/utils/utils.c
@@ -9,24 +9,27 @@ static bool    ft_isspace2(char *str)
 {
     int i;
 
-    while (str[i])
+    i = -1;
+    while (str[++i])
     {
-        if (ft_isspace(str[i], NULL))
-            return (true);
+        if (!ft_isspace(str[i], NULL))
+            return (false);
     }
-    return (false);
+    return (true);
 }
 bool	ft_isspace(int c, char *str)
 {
     if (str)
+    {
         return (ft_isspace2(str));
+    }
 	return (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v'
 		|| c == '\f');
 }
 
 bool ft_isword(int c)
 {
-    return (ft_isalpha(c) || ft_isalnum || c == '$');
+    return (ft_isalpha(c) || ft_isalnum(c) || c == '$');
 }
 
 t_all   *get_core(void)
