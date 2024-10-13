f8cebb5883ca6c9c514f3da34955f605ae0523d7
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-13 16:30:39 +0100

    add printer to print content of the lexer linked list as debugging

--- source/minishell.c
+++ source/minishell.c
@@ -2,10 +2,13 @@
 
 int main(int ac, char *av[], char *env[])
 {
-	(void *)ac;
-	(void *)av;
+	(void)ac;
+	(void)av;
 	parsing(env);
 
+	t_all *core = get_core();
+	for(t_lx *lx = core->lexer; lx; lx = lx->next)
+		printf("lx type -> %C\tvalue ->%s\n", lx->type, lx->content);
 	
 	return 0;
 }
