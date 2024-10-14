8604cf207d08a96868dbbc4a17a2b612b1bd92aa
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-14 19:05:54 +0100

    - Reformatting the struct members for better readability - Adding appropriate spacing and indentation
    These changes improve the code's organization and maintainability.

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,53 +11,54 @@
 
 typedef struct s_lx
 {
-    unsigned char type;
-    char *content;
-    struct s_lx *next;
+    unsigned char   type;
+    char            *content;
+    struct s_lx     *next;
 }   t_lx;
 
 typedef struct s_env
 {
-    char *key;
-    char *value;
-    struct s_env *next;
+    char            *key;
+    char            *value;
+    struct s_env    *next;
 }       t_env;
 
 typedef struct s_cmd
 {
-    char type;
-    char *delimiter;
-    char **cmd;
-    char *infile;
-    char *outfile;
+    char            type;
+    char            *delimiter;
+    char            **cmd;
+    char            *infile;
+    char            *outfile;
 
 }       t_cmd;
 
 typedef struct s_all
 {
-    t_env *env_list;
-    char **env;
+    t_cmd           **cmd_scope;
+    t_env           *env_list;
+    t_lx            *lexer;
+    t_gc            *gc;
     unsigned int    cmd_count;
     unsigned int    pipe_count;
-    char *in_line;
-    size_t inline_len;
-    t_cmd  **cmd_scope;
-    t_gc    *gc;
-    t_lx *lexer;
-    unsigned char exit_code;
+    unsigned char   exit_code;
+    char            **env;
+    char            *in_line;
+    size_t          inline_len;
 }       t_all;
 
 
 
-void    check_quotes(char *line);
-void    load_elements(char *line);
-void    parsing(char *env[]);
-long long lexer_add_token(char type);
-long long lexer_add_word(char type, char *line);
-
-
-
-
+/*          >Parsing Funtions<           */
+/**
+ * @brief loads all elements from the line into lexer
+ * @param line the readline input line
+ */
+void        load_elements(char *line);
+void        parsing(char *env[]);
+void        check_quotes(char *line);
+long long   lexer_add_token(char type);
+long long   lexer_add_word(char type, char *line);
 
 
 
@@ -67,12 +68,12 @@ long long lexer_add_word(char type, char *line);
  * @brief this function called whatever you need to use the main struct, its like a global varibale
  * @return refrence the t_core struct
  */
-t_all   *get_core(void);
-bool     ft_isword(int c);
-bool     ft_isspace(int c, char *str);
-bool     ft_istoken(int c);
-char    *strtkr_gen(char type);
-void    reader_loop(void);
+t_all       *get_core(void);
+bool        ft_isword(int c);
+bool        ft_isspace(int c, char *str);
+bool        ft_istoken(int c);
+char        *strtkr_gen(char type);
+void        reader_loop(void);
 
 
 
