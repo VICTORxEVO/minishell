368c56fbc8d837e5850c12b6435c60c66b069136
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-14 13:23:37 +0100

    update reader_loop to void and make in in main function inside the infinite while

--- source/parser/parsing.c
+++ source/parser/parsing.c
@@ -1,17 +1,21 @@
 #include "minishell.h"
 
-static char *reader_loop(void)
+void    reader_loop(void)
 {
-    char *line;
+    char  *line;
 
     while (true)
     {
         line = readline("minishell> ");
         if (line == NULL || line[0] == 0 || ft_isspace(0, line))
             continue;
-        return (line);
+            
+        get_core()->in_line = line;
+        gc_add_node(line);
+        break;
     }
-    return (NULL);
+    if (!ft_strncmp(line, "exit", ft_strlen("exit")))
+        pexit(" :done!", 1); //tmp function just for debugging and see leaks 
 }
 
 void    parsing(char *env[])
@@ -20,7 +24,6 @@ void    parsing(char *env[])
 
     core = get_core();
     core->env = env;
-    core->in_line = reader_loop();
     core->inline_len = ft_strlen(core->in_line);
     check_quotes(core->in_line);
     load_elements(core->in_line);
