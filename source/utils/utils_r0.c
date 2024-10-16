1c8ccf30ea4d1eae1fc9a1fe78dc5634a956f19c
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-16 17:55:28 +0100

    add is_pipe_err and is_token_err funtions

--- source/utils/utils_r0.c
+++ source/utils/utils_r0.c
@@ -1,5 +1,22 @@
 #include "minishell.h"
 
+bool    is_pipe_err(t_lx *lx, t_lx *next_lx)
+{
+    if (lx->type == PIPE && (!next_lx || next_lx->type == PIPE))
+        return (true);
+    return (false);
+}
+
+bool    is_token_err(t_lx *lx, t_lx *next_lx)
+{
+    if (lx->type != PIPE && is_token(lx->type))
+    {
+        if (!next_lx || is_token(next_lx->type))
+            return(true);
+    }
+    return(false);
+}
+
 bool    could_expand(char *str)
 {
     int i;
