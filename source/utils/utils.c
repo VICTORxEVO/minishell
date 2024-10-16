a46a6e43ba1037346e461c8b8d88704b072ec6ef
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-16 17:55:44 +0100

    chnage is_token funtion name

--- source/utils/utils.c
+++ source/utils/utils.c
@@ -1,40 +1,41 @@
 #include "minishell.h"
 
-bool           ft_istoken(int c)
+bool	is_token(int c)
 {
-    return (c == '>' || c == '<' || c == '|' || (c >= HERE_DOC && c <= OUT_RDRT_APP));
+	return (c == '>' || c == '<' || c == '|' || (c >= HERE_DOC
+			&& c <= OUT_RDRT_APP) || c == PIPE);
 }
 
-static bool    ft_isspace2(char *str)
+static bool	ft_isspace2(char *str)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while (str[++i])
-    {
-        if (!ft_isspace(str[i], NULL))
-            return (false);
-    }
-    return (true);
+	i = -1;
+	while (str[++i])
+	{
+		if (!ft_isspace(str[i], NULL))
+			return (false);
+	}
+	return (true);
 }
 bool	ft_isspace(int c, char *str)
 {
-    if (str)
-    {
-        return (ft_isspace2(str));
-    }
+	if (str)
+	{
+		return (ft_isspace2(str));
+	}
 	return (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v'
 		|| c == '\f');
 }
 
-bool ft_isword(int c)
+bool	ft_isword(int c)
 {
-    return (ft_isalpha(c) || ft_isalnum(c) || c == '$');
+	return (ft_isalpha(c) || ft_isalnum(c) || c == '$');
 }
 
-t_all   *get_core(void)
+t_all	*get_core(void)
 {
-    static t_all core;
+	static t_all	core;
 
-    return (&core);
+	return (&core);
 }
