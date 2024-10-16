45d8b9789e75571b383c5ae7f2b209846d8b1e0b
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-16 17:49:17 +0100

    Refactor macros.h to include additional token types and add error messages

--- includes/macros.h
+++ includes/macros.h
@@ -10,8 +10,8 @@
 # define MEM_ERR ": malloc"
 # define DQ_ERR ": syntax error: unclosed token `\"'"
 # define SQ_ERR ": syntax error: unclosed token `''"
-# define PIPE_ERR ": syntax error: unclosed token `|'"
-
+# define PIPE_ERR ": syntax error: empty pipe `|'"
+# define TOKEN_ERR ": sytax_error: unexpected token after `"
 
 /**
  * @brief macros for "clear" function
@@ -56,10 +56,10 @@
 #define WORD_S_QUOTES 12
 
 // pipe type token
-# define PIPE 20
+# define PIPE -1
 
 // any token type
-# define TOKEN 21
+# define TOKEN -2
 
 
 
@@ -72,7 +72,53 @@
 
 
 
+#ifndef COLORS_H
+# define COLORS_H
 
+// Regular text colors
+#define BLACK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define WHITE "\033[0;37m"
+
+// Bold text colors
+#define BOLD_BLACK "\033[1;30m"
+#define BOLD_RED "\033[1;31m"
+#define BOLD_GREEN "\033[1;32m"
+#define BOLD_YELLOW "\033[1;33m"
+#define BOLD_BLUE "\033[1;34m"
+#define BOLD_MAGENTA "\033[1;35m"
+#define BOLD_CYAN "\033[1;36m"
+#define BOLD_WHITE "\033[1;37m"
+
+// Underlined text colors
+#define UNDERLINE_BLACK "\033[4;30m"
+#define UNDERLINE_RED "\033[4;31m"
+#define UNDERLINE_GREEN "\033[4;32m"
+#define UNDERLINE_YELLOW "\033[4;33m"
+#define UNDERLINE_BLUE "\033[4;34m"
+#define UNDERLINE_MAGENTA "\033[4;35m"
+#define UNDERLINE_CYAN "\033[4;36m"
+#define UNDERLINE_WHITE "\033[4;37m"
+
+// Background colors
+#define BG_BLACK "\033[40m"
+#define BG_RED "\033[41m"
+#define BG_GREEN "\033[42m"
+#define BG_YELLOW "\033[43m"
+#define BG_BLUE "\033[44m"
+#define BG_MAGENTA "\033[45m"
+#define BG_CYAN "\033[46m"
+#define BG_WHITE "\033[47m"
+
+// Reset color
+#define END "\033[0m"
+
+#endif // COLORS_H
 
 
 
