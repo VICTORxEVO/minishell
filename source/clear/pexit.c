c095c725f1532bb5a6afab233921d4ad0b667d23
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-16 17:51:21 +0100

    remove exit temprery for easy debugging

--- source/clear/pexit.c
+++ source/clear/pexit.c
@@ -2,11 +2,12 @@
 
 void    pexit(char *msg, int exit_code)
 {
-    clear(F_ALL);
     ft_putstr_fd(PRGM_NAME, 2);
     if (msg[0] == ':')
         (ft_putstr_fd(msg, 2), ft_putstr_fd("\n", 2));
     else
         perror(msg);
-    exit (exit_code);
+    clear(F_ALL);
+    (void)exit_code;
+    // exit (exit_code);
 }
