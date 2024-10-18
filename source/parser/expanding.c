e08f72e26b939add1ab280e6ca25d9eec5b54d85
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-18 16:18:40 +0100

    fix expanding and handle more complex cases

--- source/parser/expanding.c
+++ source/parser/expanding.c
@@ -5,14 +5,14 @@ static int get_var(char *dollar_str, char *str)
     t_var *list;
 
     list = handle_list();
-    if (str - dollar_str == 1 && *str == '$')
+    if (str == dollar_str + 1 && *str == '$')
     {
         list->content = ft_itoa(getpid());
         return (ft_strlen(list->content));
     }
     else
     {
-        list->content = getenv(ft_substr(dollar_str, 1, (str - dollar_str) - 1));
+        list->content = getenv(ft_substr(dollar_str, 1,(str - dollar_str) - 1));
         if (list->content)
             return (ft_strlen(list->content));
         return (0);
@@ -34,11 +34,13 @@ static char *calc_max_len(char *str)
         str = new_str + 1;
         while (*str && *str != '$' && !ft_isspace(*str, NULL))
             str++;
-        if (str == new_str + 1)
+        if (str == new_str + 1 && (!*str || ft_isspace(*str, NULL)))
             len += 1;
         else
             len += get_var(new_str, str);
-        new_str = get_dollar(new_str);
+        if (*str == '$')
+            new_str += 1;
+        new_str = get_dollar(new_str + 1);
     }
     printf("final len is %d\n", len);
     return (galloc(sizeof(char) * len + 1));
@@ -47,7 +49,10 @@ static char *calc_max_len(char *str)
 
 static void    dq_word_expander(t_lx *lx)
 {
-    (void)lx;
+    char *new_str;
+
+    new_str = calc_max_len(lx->content);
+    (void)new_str;
 }
 
 static void     word_expander(t_lx *lx)
