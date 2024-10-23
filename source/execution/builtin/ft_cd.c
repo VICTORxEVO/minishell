a5bf62e932ae4e2ad87bb0dc682bdc6cbd3be858
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-23 12:16:41 +0100

    source/execution/builtin/ft_cd.c

--- source/execution/builtin/ft_cd.c
+++ source/execution/builtin/ft_cd.c
@@ -34,16 +34,26 @@ int ft_cd(t_cmd * cmd)
     
     if (!getcwd(cwd, sizeof(cwd)))
         return (-1);
-    if (ft_strcmp(cmd->cmd[1], "~") == 0) {
+    if (!cmd->cmd[1] || ft_strcmp(cmd->cmd[1], "~") == 0) {
     {
         update_env_cwd(cwd);
-        chdir(getenv("HOME"));
+        chdir(ft_getenv("HOME"));
     }
     } else if (ft_strcmp(cmd->cmd[1], "-") == 0)
     {
-        owd = update_env_cwd(cwd);
-        chdir(owd);
-    } else
+        if (chdir(owd) == 0)
+            owd = update_env_cwd(cwd);
+        else
+        {
+            //to add, program name instead of "bash"
+            printf("bash: cd: %s: No such file or directory\n", owd);
+        }
+    }
+    else if (ft_strcmp(cmd->cmd[1], "."))
+    {
+
+    }
+    else if (ft_strcmp(cmd->cmd[1], ".."))
     {
         if (chdir(cmd->cmd[1]) != 0) {
             perror("error"); // Print error if chdir fails
