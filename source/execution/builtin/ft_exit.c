e92804c29dea1b60c69f1e1cc344fa495f3c6a63
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-23 12:17:59 +0100

    extra builtin files, to be done with later

---
+++ source/execution/builtin/ft_exit.c
@@ -0,0 +1,13 @@
+
+
+
+
+
+#include "minishell.h"
+
+
+void    ft_exit(t_cmd *cmd)
+{
+    (void)cmd;
+    printf("is exit\n");
+}
---
+++ source/execution/builtin/ft_export.c
@@ -0,0 +1,20 @@
+
+
+
+
+#include "minishell.h"
+
+
+int    ft_export(t_cmd *cmd)
+{
+    (void) cmd;
+    printf("is export\n");
+    return (1);
+}
+
+bool    is_in_env(t_cmd *cmd)
+{
+    //to do 
+    (void) cmd;
+    return (1);
+}
