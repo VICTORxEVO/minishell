e13503a967a270cc41cf927a1bab781bced57de3
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-23 07:58:08 +0100

    ft_unset(char*) returns 1 if nothing found ? to check

--- source/execution/builtin/ft_unset.c
+++ source/execution/builtin/ft_unset.c
@@ -7,8 +7,17 @@
 #include "minishell.h"
 
 
-void    ft_unset(char *cmd)
+int    ft_unset(t_cmd *cmd)
 {
-    printf("is unset\n");
+    t_env *env;
+
+    env = get_core()->env_list;
+    while (env && cmd->cmd[1])
+    {
+        if (ft_strcmp(env->key, cmd->cmd[1]))
+            return(ft_remove_node(env, cmd->cmd[1], 0));
+        env = env->next;
+    }
+    return (1);
 }
 
