2f06145d5b899ffe49f7312a7f0841504fa5b12e
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-23 12:19:20 +0100

    COMPLETE THE EXPANDING

--- source/parser/expanding.c
+++ source/parser/expanding.c
@@ -4,73 +4,88 @@ static int get_var(char *dollar_str, char *str)
 {
     t_var *list;
 
+    if (str == dollar_str + 1 && !*str)
+        return (1);
     list = handle_list();
+    list->start_addr = dollar_str;
+    list->end_addr = str;
     if (str == dollar_str + 1 && *str == '$')
     {
         list->content = ft_itoa(getpid());
+        list->end_addr = str + 1;
         return (ft_strlen(list->content));
     }
-    else
+    else if (str == dollar_str + 1 && *str == '?')
     {
-        list->content = getenv(ft_substr(dollar_str, 1,(str - dollar_str) - 1));
-        if (list->content)
-            return (ft_strlen(list->content));
-        return (0);
+        list->content = ft_itoa(get_core()->exit_code);
+        list->end_addr = str + 1;
+        return (ft_strlen(list->content));
     }
+    list->content = getenv(ft_substr(dollar_str, 1,(str - dollar_str) - 1));
+    return (ft_strlen(list->content));
 }
 
 static char *calc_max_len(char *str)
 {
-    char *new_str;
+    char *dollar_str;
     int len;
 
     len = 0;
-    new_str = get_dollar(str);
-    len += new_str - str;
-    while (new_str)
+    dollar_str = get_dollar(str);
+    len += dollar_str - str;
+    while (dollar_str)
     {
-        if (*str != '$')
-            len += new_str - str;
-        str = new_str + 1;
-        while (*str && *str != '$' && !ft_isspace(*str, NULL))
+        if ((*str == '$' && str > dollar_str + 1))
+            len += dollar_str - str;
+        str = dollar_str + 1;
+        while (*str && !possible_expand(*str))
             str++;
-        if (str == new_str + 1 && (!*str || ft_isspace(*str, NULL)))
-            len += 1;
-        else
-            len += get_var(new_str, str);
-        if (*str == '$')
-            new_str += 1;
-        new_str = get_dollar(new_str + 1);
+        len += get_var(dollar_str, str);
+        if (str == dollar_str + 1 && *str == '$')
+            dollar_str += 1;
+        dollar_str = get_dollar(dollar_str + 1);
     }
+    dollar_str = get_end_addr(str);
+    len += dollar_str - str;
     printf("final len is %d\n", len);
     return (galloc(sizeof(char) * len + 1));
-}
-
-
-static void    dq_word_expander(t_lx *lx)
-{
-    char *new_str;
-
-    new_str = calc_max_len(lx->content);
-    (void)new_str;
 }
 
 static void     word_expander(t_lx *lx)
 {
     char *new_str;
+    char *str;
+    t_var *list;
+    int size;
 
-    new_str = calc_max_len(lx->content);
-    (void)new_str;
+    str = lx->content;
+    new_str = calc_max_len(str);
+    list = get_core()->var_list;
+    while (list)
+    {
+        size = list->start_addr - str;
+        ft_memcpy(new_str + ft_strlen(new_str), str, size);
+        ft_memcpy(new_str + ft_strlen(new_str), list->content, ft_strlen(list->content));
+        str = (char *)list->end_addr;
+        list = list->next;
+    }
+    ft_memcpy(new_str + ft_strlen(new_str), str, get_end_addr(str) - str);
+    printf("new lexer str -> %s\\0\n", new_str);
+}
+static void split_content(char *lx_str, t_lx *lexer)
+{
+    (void)lx_str;
+    (void)lexer;
 }
 
 void    expand_dollar(t_lx *lexer)
 {
     while(lexer)
     {
-        if (lexer->type == WORD && could_expand(lexer->content))
+        if (could_expand(lexer->content))
             word_expander(lexer);
-        else if (lexer->type == WORD_D_QUOTES && could_expand(lexer->content))
-            dq_word_expander(lexer);
+        if (lexer->type == WORD)
+            split_content(lexer->content, lexer);
         lexer = lexer->next;
     }
 }
