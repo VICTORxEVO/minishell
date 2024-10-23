147e4fb2c776fe2cbe6a8949b57407853505b5cc
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-23 09:11:33 +0100

    added remove_node() func to remove node from t_env list

--- source/execution/builtin/ft_unset.c
+++ source/execution/builtin/ft_unset.c
@@ -7,6 +7,31 @@
 #include "minishell.h"
 
 
+void    ft_remove_node(char *node)
+{
+    t_env *env;
+    t_env *prev;
+
+    env = get_core()->env_list;
+    prev = NULL;
+    while (env)
+    {
+        if (ft_strcmp(env->key, node) == 0)
+        {
+            if (prev == NULL)
+                get_core()->env_list = env->next;
+            else
+                prev->next = env->next;
+            free(env->key);
+            free(env->value);
+            free(env);
+            break;
+        }
+        prev = env;
+        env = env->next;
+    }
+}
+
 int    ft_unset(t_cmd *cmd)
 {
     t_env *env;
@@ -15,7 +40,7 @@ int    ft_unset(t_cmd *cmd)
     while (env && cmd->cmd[1])
     {
         if (ft_strcmp(env->key, cmd->cmd[1]))
-            return(ft_remove_node(env, cmd->cmd[1], 0));
+            return (ft_remove_node(cmd->cmd[1]), 0);
         env = env->next;
     }
     return (1);
