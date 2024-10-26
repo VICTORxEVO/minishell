d7f23f7c201bc84ad3a5d8c0163cdb18c0219b7b
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-10-26 11:06:15 +0100

    added : ft_add_node() + ft_setenv()

--- source/execution/builtin/ft_unset.c
+++ source/execution/builtin/ft_unset.c
@@ -7,6 +7,25 @@
 #include "minishell.h"
 
 
+int  ft_add_node(t_env *last_node,char *new_key, char *new_val)
+{
+    t_env *env;
+
+    if (!last_node)
+        return (0);
+    env = malloc(sizeof(t_env));
+    if (!env)
+        return (0);
+    env->key = ft_strdup(new_key);
+    env->value = ft_strdup(new_val);
+    env->next = NULL;
+    if (last_node && last_node->next == NULL)
+        last_node->next = env;
+    else
+        return (free(env), 0);
+    return (1); 
+}
+
 void    ft_remove_node(char *node)
 {
     t_env *env;
@@ -31,6 +50,28 @@ void    ft_remove_node(char *node)
         env = env->next;
     }
 }
+
+ int    ft_setenv(char *key, char *val, int overwrite)
+ {
+    t_env *env;
+    t_env *prev;
+
+    env = get_core()->env_list;
+    prev = env;
+    while (env)
+    {
+        if (ft_strcmp(key, env->key) == 0)
+        {
+            free(env->value);
+            env->value = ft_strdup(val);
+            break;
+        }
+        prev = env;
+        env = env->next;
+    }
+    ft_add_node(prev,key ,val);
+ }
+
 
 int    ft_unset(t_cmd *cmd)
 {
