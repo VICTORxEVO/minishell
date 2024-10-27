cb8cae5cf1d211093546719b467751dc2e575c0c
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-27 17:10:13 +0100

    Fix ft_strncmp  and Improve it function to handle negative values for n

--- source/external/ft_strncmp.c
+++ source/external/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ysbai-jo <ysbai-jo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 13:28:30 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2024/10/10 21:00:51 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2024/10/27 16:14:52 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	while ((*str1 || *str2) && n--)
+	if (n < 0)
+	{
+		while ((*str1 && *str2))
+		{
+			if (*str1 == *str2)
+			{
+				str1++;
+				str2++;
+			}
+			else
+				return (*(t_uc *)str1 - *(t_uc *)str2);
+		}
+	}
+	while ((*str1 && *str2) && n--)
 	{
 		if (*str1 == *str2)
 		{
