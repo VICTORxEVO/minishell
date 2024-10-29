dfb4094bed16616f9d68592c640c8cba794860af
VICTOR EVO <yassirsbai04@gmail.com>
2024-10-29 20:45:46 +0100

    add clear_1data func

---
+++ source/clear/clear1.c
@@ -0,0 +1,18 @@
+#include "minishell.h"
+
+void clear_1data(void *data)
+{
+    t_gc *list;
+
+    list = getcore()->gc;
+    while(list)
+    {
+        if (list->data == data)
+        {
+            safe_free((void **)data);
+            return ;
+        }
+        list = list->next;
+    }
+    pexit(PTR_ERR, 101);
+}
