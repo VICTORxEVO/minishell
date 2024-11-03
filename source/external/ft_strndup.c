51d58b6bbb1b320f7ea5cc27b958200cf99a070b
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-11-03 21:54:18 +0100

    increament by one: strndup

--- source/external/ft_strndup.c
+++ source/external/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: ael-moha <ael-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 21:31:45 by ael-moha          #+#    #+#             */
-/*   Updated: 2024/11/03 21:44:05 by ael-moha         ###   ########.fr       */
+/*   Updated: 2024/11/03 21:52:19 by ael-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char *ft_strndup(const char *s, size_t n) {
 
     len = ft_strlen(s);
     if (n < len)
-        len = n;
+        len = n + 1;
     dup = (char *)galloc(len + 1);
     if (!dup)
         return NULL;
