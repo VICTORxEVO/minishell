0e33f46a4e1df6e56e3fb288fdcd4a2ba2487d95
VICTOR EVO <yassirsbai04@gmail.com>
2024-11-05 12:49:46 +0100

    remove printing from main

--- source/minishell.c
+++ source/minishell.c
@@ -8,20 +8,14 @@ int main(int ac, char *av[], char *env[])
 	{
 		reader_loop();
 		parsing(env);
-
-		t_lx *lexer = getcore()->lexer;
-		t_var *list = getcore()->var_list;
-		while(lexer)
-		{
-			printf("lx type -> %d, lx content ->\t%s\\0\n", lexer->type, lexer->content);
-			lexer = lexer->next;
-		}
-		printf("---------------\n");
-		while (list)
-		{
-        	printf ("list content is ->\t%s\\0\n", list->content);
-        	list = list->next;
-		}
+        // addoldhistory();
+		// t_var *list = getcore()->var_list;
+		// printf("---------------\n");
+		// while (list)
+		// {
+        // 	printf ("list content is ->\t%s\\0\n", list->content);
+        // 	list = list->next;
+		// }
 		clear(F_TMP);
 	}
 	
