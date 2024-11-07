503000da92ba22d45681e1c901fb1c6d5d640a58
VICTOR EVO <yassirsbai04@gmail.com>
2024-11-07 17:56:11 +0100

    refactor reader_loop function to improve prompt handling and memory management refactor parsing funtion and have funtion to debug lexer and env

--- source/parser/parsing.c
+++ source/parser/parsing.c
@@ -1,5 +1,30 @@
 #include "minishell.h"
 
+void    reader_loop(void)
+{
+    char  *line;
+    char  *previous_line;
+    char *prompt;
+
+    while (true)
+    {
+        prompt = create_prompt();
+        line = readline(prompt);
+        if (line == NULL || line[0] == 0 || ft_isspace(0, line))
+        {
+            if (line)
+                free(line);
+            continue;
+        }
+        getcore()->in_line = line;
+        gc_add_node(line);
+        previous_line = getcore()->previous_line;
+        add_history(line);
+        break ;
+    }
+    if (!ft_strncmp(line, "exit", ft_strlen("exit")))
+        pexit(": done!", 1); //tmp function just for debugging and see leaks 
+}
 static void    lexing(char *line)
 {
     long long i;
@@ -24,86 +49,41 @@ static void    lexing(char *line)
     }
 }
 
-void    reader_loop(void)
+
+static void   fill_env_list(char *env[])
 {
-    char  *line;
-    char  *previous_line;
+    t_env *env_node;
+    int i;
 
-    while (true)
+    i = -1;
+    while (env[++i])
     {
-        line = readline(BOLD_MAGENTA"Eureka> "END);
-        if (line == NULL || line[0] == 0 || ft_isspace(0, line))
-            continue;
-            
-        getcore()->in_line = line;
-        gc_add_node(line);
-        previous_line = getcore()->previous_line;
-        if (!(previous_line && !ft_strncmp(line, previous_line, -1)))
-            add_history(line);
-        break;
+        env_node = ft_calloc(1, sizeof(t_env));
+        env_node->key = strchrdup(env[i], ft_strchr(env[i], '='), CALLOC);
+        env_node->value = strchrdup(ft_strchr(env[i], '=') + 1, NULL, CALLOC);
+        addtolist(env_node, "t_env");
     }
-    if (!ft_strncmp(line, "exit", ft_strlen("exit")))
-        pexit(": done!", 1); //tmp function just for debugging and see leaks 
-}
-
-static t_env   *fill_env_list(char *env[])
-{
-    t_env *env_list;
-    t_env *head;
-    char **key_val;
-    t_ndx index;
-
-    ft_bzero(&index, sizeof(t_ndx));
-    while (env[index.i])
-    {
-        if (index.i == 0)
-        {
-            env_list = ft_calloc(1, sizeof(t_env));
-            head = env_list;
-        }
-        else
-        {
-            while (env_list->next)
-                env_list = env_list->next;
-            env_list->next = ft_calloc(1, sizeof(t_env));
-            env_list = env_list->next;
-        }
-        key_val = ft_split(env[index.i++], '=');
-        env_list->key =  key_val[0];
-        env_list->value = key_val[1];
-    }
-    return (head);
 }
 void    parsing(char *env[])
 {
     t_all *core;
 
     core = getcore();
-    core->env_list = fill_env_list(env);
+    fill_env_list(env);
     core->inline_len = ft_strlen(core->in_line);
     check_quotes(core->in_line);
-    printf("line before->%s\n", core->in_line);
     lexing(core->in_line);
-    t_lx *lexer = getcore()->lexer;
-	while(lexer)
-	{
-		printf("lx type -> %d, lx content ->\t%s\\0\n", lexer->type, lexer->content);
-		lexer = lexer->next;
-	}
-    printf("---------------------------\n");
+    // print_env();
+    print_lx();
     if (could_expand(core->in_line))
     {
+        pexit(": feature not availble yet, please try another thing!", 1);
         core->in_line = expand_dollar(core->in_line);
         lexing(core->in_line);
         printf("line after->%s\n", core->in_line);
-        lexer = getcore()->lexer;
-	    while(lexer)
-	    {
-		    printf("lx type -> %d, lx content ->\t%s\\0\n", lexer->type, lexer->content);
-		    lexer = lexer->next;
-	    }
+        print_lx();
     }
-    // check_syntax(core->lexer);
+    check_syntax(core->lexer);
     // final_touch(core->lexer); // remove all kind of quotes in begging or in middle 
-    // load_cmd_list(core);
+    load_cmd_list(core);
 }
