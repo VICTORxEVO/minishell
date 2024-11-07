5ff7e836914aa0fabcdba23ebc41001f29fc1e6f
VICTOR EVO <yassirsbai04@gmail.com>
2024-11-07 17:53:56 +0100

    remove commented-out code  in main

--- source/minishell.c
+++ source/minishell.c
@@ -8,14 +8,6 @@ int main(int ac, char *av[], char *env[])
 	{
 		reader_loop();
 		parsing(env);
-        // addoldhistory();
-		// t_var *list = getcore()->var_list;
-		// printf("---------------\n");
-		// while (list)
-		// {
-        // 	printf ("list content is ->\t%s\\0\n", list->content);
-        // 	list = list->next;
-		// }
 		clear(F_TMP);
 	}
 	
