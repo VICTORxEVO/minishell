0d913fc59986e76ee6dcb6ad17e5171494ecc511
Yassir Sbai <yassirsbai04@gmail.com>
2024-11-16 14:37:45 +0100

    add some expanding funtions

--- source/parser/expanding1.c
+++ source/parser/expanding1.c
@@ -1,5 +1,34 @@
 #include "minishell.h"
 
+t_lx    *splitcontent(char *str)
+{
+    
+}
+
+t_lx    *addnewlist(t_lx *new_list, t_lx *lexer, t_lx *prev_lx, t_lx *next_lx)
+{
+    t_lx *newlisttail;
+
+    while(newlisttail->next)
+        newlisttail = newlisttail->next;
+    if (!prev_lx)
+    {
+        getcore()->lexer = new_list;
+        newlisttail->next = next_lx;
+        next_lx->prev = newlisttail;
+    }
+    else
+    {
+        clear_1data(lexer->content);
+        clear_1data(lexer);
+        prev_lx->next = new_list;
+        new_list->prev = prev_lx;
+        newlisttail->next = next_lx;
+        next_lx->prev = newlisttail;
+    }
+    return (new_list);
+}
+
 int    get_var_special(int *index, char type)
 {
     t_var *list;
