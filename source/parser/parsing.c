a3968dc7c37313c40e34d8435f33f7e95b642caa
Yassir Sbai <yassirsbai04@gmail.com>
2024-11-16 14:38:30 +0100

    refactor parsing order

--- source/parser/parsing.c
+++ source/parser/parsing.c
@@ -31,21 +31,18 @@ void    parsing(char *env[])
     t_all *core;
 
     core = getcore();
-    fill_env_list(env);
     core->inline_len = ft_strlen(core->in_line);
     check_quotes(core->in_line);
     lexing(core->in_line);
     // print_env();
     print_lx();
-    // core->in_line = heredoc_case(getcore()->lexer);
-    if (could_expand(core->in_line))
+    if (needexpand(core->in_line))
     {
-        pexit(": feature not availble yet, please try another thing!", 1);
-        core->in_line = expand_dollar(core->in_line);
-        lexing(core->in_line);
-        printf("line after->%s\n", core->in_line);
-        print_lx();
+        printf("this feature is not cooked right yet, please try again\n");
+        // expanding(core->lexer);
     }
-    // final_touch(core->lexer); // remove all kind of quotes in begging or in middle 
-    // load_cmd_list(core);
+    printf("line after->%s\n", core->in_line);
+    print_lx();
+    final_touch(core->lexer); // remove all kind of quotes in begging or in middle 
+    load_cmd_list(core);
 }
