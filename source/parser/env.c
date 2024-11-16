a6e57615f934a40486904548e0c6d89a4920477b
Yassir Sbai <yassirsbai04@gmail.com>
2024-11-16 14:36:35 +0100

    add fill_path funtion

--- source/parser/env.c
+++ source/parser/env.c
@@ -1,5 +1,44 @@
 #include "minishell.h"
 
+static char	*ft_custumjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!new)
+		return (NULL);
+	ft_memcpy(new, s1, s1_len);
+	ft_strlcpy(&new[s1_len], s2, s2_len + 1);
+	return (new);
+}
+
+static char **setup_path(char **path)
+{
+    int i;
+    char **newpath;
+
+    i = 0;
+    while (path[i])
+        i++;
+    newpath = ft_calloc(i + 1, sizeof(char *));
+    newpath[i] = NULL;
+    i = -1;
+    while (path[++i])
+        newpath[i] = ft_custumjoin(path[i], "/");
+    return (newpath);
+}
+
+void    fill_path(void)
+{
+    
+}
+
 void   fill_env_list(char *env[])
 {
     t_env *env_node;
