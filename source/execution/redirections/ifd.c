80f1a1cea292893f7b98b0e951d600e6f2e7531b
Yassir Sbai <yassirsbai04@gmail.com>
2024-11-21 10:10:39 +0100

    add: ifd func

--- source/execution/redirections/ifd.c
+++ source/execution/redirections/ifd.c
@@ -1,13 +1,6 @@
 #include "minishell.h"
 
-int     redirection(char *filename, char mode, int oldfd)
+int     ifd(char *filename)
 {
-    if (mode == HERE_DOC)
-        return (hd(filename, oldfd));
-    else if (mode == IN_RDRT)
-        return (ifd(filename));
-    else
-        return (ofd(filename, mode));
-    if (oldfd > 2)
-        close (oldfd);
+    (void)filename;
 }
