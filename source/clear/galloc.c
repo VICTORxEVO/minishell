14d91c2ab8aa3c7a88b568613434a9060de8af70
Yassir Sbai <yassirsbai04@gmail.com>
2024-11-22 21:22:54 +0100

    fix: update error message in memory allocation functions for clarity

--- source/clear/galloc.c
+++ source/clear/galloc.c
@@ -26,7 +26,7 @@ inline t_gc	*gc_new(void *data)
 
 	new = ft_calloc(1, sizeof(t_gc));
 	if (!new)
-		pexit(MEM_ERR, 1);
+		pexit("malloc", 1);
     new->data = data;
 	return (new);
 }
@@ -59,7 +59,7 @@ inline void *galloc(size_t size)
 
     data = ft_calloc(1, size);
     if (!data)
-        (pexit(MEM_ERR, 1));
+        (pexit("malloc", 1));
     gc_add_node(data);
     return (data);
 }
