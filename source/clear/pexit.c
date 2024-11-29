26a57da03f92816421a00ffe5192ab0bcee410d6
Yassir Sbai <yassirsbai04@gmail.com>
2024-11-29 19:07:44 +0100

    fix: change exit_code type in pexit func to short int and update error handling

--- source/clear/pexit.c
+++ source/clear/pexit.c
@@ -1,13 +1,13 @@
 #include "minishell.h"
 
-void    pexit(char *msg, int exit_code)
+void    pexit(char *msg, short int exit_code)
 {
     ft_putstr_fd(PRGM_NAME, 2);
     if (msg[0] == ':')
         (ft_putstr_fd(msg, 2), ft_putstr_fd("\n", 2));
     else
         (ft_putstr_fd(":", 2), perror(msg));
-    clear(FREE_ALL);
-    (void)exit_code;
-    exit (exit_code);
+    if (exit_code > 0)
+        getcore()->error_flag = true;
+    getcore()->exit_code = (unsigned char)exit_code;
 }
