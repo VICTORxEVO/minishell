481dd479f2aac32032ecc345f40d0c431487f4b1
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-03 09:10:48 +0100

    feat: add duping function for handling file descriptor duplication in ifd.c

--- source/execution/redirections/ifd.c
+++ source/execution/redirections/ifd.c
@@ -1,5 +1,22 @@
 #include "minishell.h"
 
+bool    duping(int ifd, int ofd)
+{
+    if (ifd > 2)
+    {
+        if (dup2(ifd, STDIN_FILENO) < 0)
+            return (pexit("dup2", DUP2_CODE), false);
+        close(ifd);
+    }
+    if (ofd > 2)
+    {
+        if (dup2(ofd, STDOUT_FILENO) < 0)
+            return (pexit("dup2", DUP2_CODE), false);
+        close(ofd);
+    }
+    return(true);
+}
+
 int     ifd(char *filename)
 {
     int fd;
