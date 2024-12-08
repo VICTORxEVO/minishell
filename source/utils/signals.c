c833aa8879d1c8fd88d26e33ceccf662eef91cf6
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-12-08 17:26:35 +0100

    init signals

--- source/utils/signals.c
+++ source/utils/signals.c
@@ -3,5 +3,37 @@
 #include "minishell.h"
 
 
+void	sigint_handler_in_process(int sig)
+{
+	(void) sig;
+	printf("\n");
+}
 
+void	sigquit_handler_in_process(int sig)
+{
+	(void) sig;
+	printf("Quit: %d\n", sig);
+}
 
+void	sigint_handler_nonl(int sig)
+{
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	(void) sig;
+}
+
+void	sigint_handler(int sig)
+{
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	(void) sig;
+}
+
+void	signals(void)
+{
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
