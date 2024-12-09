7372eb93cd451c321e8f8df13eab67900e00e542
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-09 15:54:17 +0100

    fix: improve error message formatting in pexit function

--- source/clear/pexit.c
+++ source/clear/pexit.c
@@ -6,7 +6,7 @@ void    pexit(char *msg, short int exit_code)
     if (msg[0] == ':')
         (ft_putstr_fd(msg, 2), ft_putstr_fd("\n", 2));
     else
-        (ft_putstr_fd(":", 2), perror(msg));
+        (ft_putstr_fd(": ", 2), perror(msg));
     if (exit_code > 0)
         getcore()->error_flag = true;
     getcore()->exit_code = (unsigned char)exit_code;
