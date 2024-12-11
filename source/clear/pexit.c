7f7998d8b0ea6714ea33eaac385c240f4ad14609
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-11 15:16:03 +0100

    fix: clear resources on command not found and permission denied errors in pexit function

--- source/clear/pexit.c
+++ source/clear/pexit.c
@@ -9,5 +9,7 @@ void    pexit(char *msg, short int exit_code)
         (ft_putstr_fd(": ", 2), perror(msg));
     if (exit_code > 0)
         getcore()->error_flag = true;
+    if (exit_code == CMD_NOT_FOUND_CODE || exit_code == PERM_DENIED_CODE)
+        clear(FREE_ALL);
     getcore()->exit_code = (unsigned char)exit_code;
 }
