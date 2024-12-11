82840ba8ad2ffb42ee6b5adb3c8484a1db78c4d3
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-11 15:20:33 +0100

    fix: remove unused env variable in ft_add_export function

--- source/execution/builtin/ft_export.c
+++ source/execution/builtin/ft_export.c
@@ -54,11 +54,9 @@ static int parse_key_value(const char *arg, char **key, char **value) {
 
 static int ft_add_export(char *arg, bool has_plus)
 {
-    t_env *env;
     char *key;
     char *value;
 
-    env = getcore()->env_list;
     parse_key_value(arg, &key, &value);
     if (value && has_plus)
         ft_setenv(key, value, 2); 
