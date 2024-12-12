bffbf4f149149c4fed802149162043055818318e
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-12 20:28:25 +0100

    handle builtin commands properly in execution and rename exec_1cmdparent

--- source/execution/execution1.c
+++ source/execution/execution1.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void    exec_1cmdparent(void *data, pid_t pid)
+void    exec_cmdparent(void *data, pid_t pid)
 {
     int status;
     int *i;
@@ -11,21 +11,24 @@ void    exec_1cmdparent(void *data, pid_t pid)
     getcore()->exit_code = WEXITSTATUS(status);
 }
 
-void    exec_1cmdchild(void *data)
+void    exec_cmdchild(void *data)
 {
     t_cmd *cmd;
     char *cmdpath;
+    int exitcode;
 
+    exitcode = 0;
     cmd = (t_cmd *)data;
     if (!prepare_ifof(cmd))
         (clear(FREE_ALL), exit(getcore()->exit_code));
-    if (cmd->cmd)
+    if (cmd->cmd && !is_builtin(cmd->cmd[0]))
     {
         cmdpath = getcmdpath(cmd->cmd[0]);
         execve(cmdpath, cmd->cmd, getcore()->env);
         (close(cmd->ifd), close(cmd->ofd));
         pexit("execve", 1, EXIT);
     }
+    exitcode = exec_builtin(cmd);
     (close(cmd->ifd), close(cmd->ofd));
-    (clear(FREE_ALL), exit(0));
+    (clear(FREE_ALL), exit(exitcode));
 }
