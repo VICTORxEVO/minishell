3242db1c6e73d68cd5046a8aa1c284f458767472
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-12 17:49:33 +0100

    fix: update error handling in ofd function to include a third param

--- source/execution/redirections/ofd.c
+++ source/execution/redirections/ofd.c
@@ -8,13 +8,13 @@ int     ofd(char *filename, char mode)
     {
         fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0666);
         if (fd < 0)
-            pexit(ft_strjoin("open: ", filename), OPEN_CODE);
+            pexit(ft_strjoin("open: ", filename), OPEN_CODE, 0);
     }
     else
     {
         fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0666);
         if (fd < 0)
-            pexit(ft_strjoin("open: ", filename), OPEN_CODE);
+            pexit(ft_strjoin("open: ", filename), OPEN_CODE, 0);
     }
     return (fd);
 }
