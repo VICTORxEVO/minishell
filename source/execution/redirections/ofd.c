7833edc2d1df8f04d2b2a586dfd0e8b2dd4a0da8
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-12 20:28:59 +0100

    fix: update output redirection mode in ofd function for correct file handling

--- source/execution/redirections/ofd.c
+++ source/execution/redirections/ofd.c
@@ -4,7 +4,7 @@ int     ofd(char *filename, char mode)
 {
     int fd;
 
-    if (mode == OUT_RDRT_APP)
+    if (mode == OUT_RDRT_OW)
     {
         fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0666);
         if (fd < 0)
