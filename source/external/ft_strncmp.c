174ce64631c4e404d5863d599b01df6c4327e747
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-12 17:50:48 +0100

    fix: improve ft_strncmp logic

--- source/external/ft_strncmp.c
+++ source/external/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ysbai-jo <ysbai-jo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 13:28:30 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2024/11/10 15:04:17 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2024/12/12 13:21:30 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,12 @@ int	ft_strncmp(const char *str1, const char *str2, ssize_t n)
 {
 	if (n < 0)
 	{
-		while ((*str1 || *str2))
+		while (*str1 && *str2 && (*str1 == *str2))
 		{
-			if (*str1 == *str2)
-			{
-				str1++;
-				str2++;
-			}
-			else
-				return (*(t_uc *)str1 - *(t_uc *)str2);
+			str1++;
+			str2++;
 		}
+		return (*(t_uc *)str1 - *(t_uc *)str2);
 	}
 	while ((*str1 || *str2) && n-- > 0)
 	{
