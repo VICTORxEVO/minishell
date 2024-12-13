7279e42c956b4c9a869ce7ee4ef545777211a918
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-13 19:16:46 +0100

    fix: change waiting logic for multiple commands

--- source/execution/execution1.c
+++ source/execution/execution1.c
@@ -1,14 +1,39 @@
 #include "minishell.h"
 
+void    wait_childs(pid_t *pid)
+{
+    int i;
+    int status;
+
+    i = -1;
+    while(++i < (int)getcore()->cmd_count)
+        waitpid(pid[i], &status, 0);
+    getcore()->exit_code = WEXITSTATUS(status);
+}
+
 void    exec_cmdparent(void *data, pid_t pid)
 {
     int status;
     int *i;
+    int signal ;
 
+    signal = 0;
     i = (int *)data;
-    getcore()->pids[*i] = pid;
-    waitpid(pid, &status, 0);
-    getcore()->exit_code = WEXITSTATUS(status);
+    if (i)
+    {
+        getcore()->pids[*i] = pid;
+        if (*i + 1 == (int)getcore()->cmd_count)
+            wait_childs(getcore()->pids);
+        return ;
+    }
+        wait(&status);
+        if (WIFSIGNALED(status))
+        {
+            signal = WTERMSIG(status) + 128;
+            if (WTERMSIG(status) == SIGSEGV)
+                ft_putstr_fd("Segmentation fault (core dumped)\n", 2);
+        }
+        getcore()->exit_code = WEXITSTATUS(status) + signal;
 }
 
 void    exec_cmdchild(void *data)
