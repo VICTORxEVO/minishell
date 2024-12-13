d05e66a1e54fb86765bacd7fe0e45dcf37fe5389
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-13 19:17:58 +0100

    fix: improve redirection handling and cleanup logic in redirection functions

--- source/execution/redirections/redirection.c
+++ source/execution/redirections/redirection.c
@@ -1,5 +1,21 @@
 #include "minishell.h"
 
+static void    redirection_cleanup(t_lx *lexer, int ifd, int ofd)
+{
+    char *str;
+
+    if (ifd > 2)
+        close(ifd);
+    if (ofd > 2)
+        close(ofd);
+    close_allhd(lexer);
+    if (lexer->type == AMBIG_RDRT)
+    {
+        str = ft_strjoin(ft_strjoin(": ", lexer->next->content), AMBIG_ERR);
+        pexit(str, AMBIG_CODE, 0);
+    }
+}
+
 int     redirection(char *filename, char mode, int oldfd)
 {
     if (oldfd > 2)
@@ -14,7 +30,7 @@ int     redirection(char *filename, char mode, int oldfd)
         return (ofd(filename, mode));
 }
 
-static bool prepare_heredoc(t_cmd *cmd_list)
+bool prepare_heredoc(t_cmd *cmd_list)
 {
     int fd;
     t_lx *lexer;
@@ -28,7 +44,7 @@ static bool prepare_heredoc(t_cmd *cmd_list)
             {
                 fd = redirection(lexer->next->content, lexer->type, 0);
                 if (fd < 0)
-                    return (false);
+                    return (close_allhd(getcore()->lexer), false);
                 clear_1data(lexer->next->content);
                 lexer->type = HERE_DOC_FD;
                 lexer->next->content = ft_itoa(fd);
@@ -47,16 +63,16 @@ bool    prepare_ifof(t_cmd *cmd_list)
     lexer = cmd_list->scope;
     if (cmd_list->unsed_fd > 2)
         close(cmd_list->unsed_fd);
-    if (!prepare_heredoc(cmd_list))
-        return (false);
     while (lexer)
     {   
+        if (lexer->type == AMBIG_RDRT)
+            return (redirection_cleanup(lexer, cmd_list->ifd, cmd_list->ofd), false);
         if (lexer->type == IN_RDRT || lexer->type == HERE_DOC_FD)
             cmd_list->ifd = redirection(lexer->next->content, lexer->type, cmd_list->ifd);
         else if (lexer->type == OUT_RDRT_APP || lexer->type == OUT_RDRT_OW)
             cmd_list->ofd = redirection(lexer->next->content, lexer->type, cmd_list->ofd);
         if (cmd_list->ofd < 0 || cmd_list->ifd < 0)
-            return (close_allhd(lexer), false);
+            return (redirection_cleanup(lexer, cmd_list->ifd, cmd_list->ofd), false);
         lexer = lexer->next;
     }
     if (!duping(cmd_list->ifd, cmd_list->ofd))
