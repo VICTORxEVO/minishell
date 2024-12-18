e15692ac3bda38fcffde7d564daf4cfb7df5a0e9
Yassir Sbai <yassirsbai04@gmail.com>
2024-12-18 12:58:17 +0100

    fix: modify ft_exit to conditionally print exit message based on main_pid

--- source/execution/builtin/ft_exit.c
+++ source/execution/builtin/ft_exit.c
@@ -23,5 +23,7 @@ int    ft_exit(char **cmd)
         if (exitcode > 255)
             return(pexit(EXIT_USAGE, 2, 0), 2);
     }
-    (printf("exit\n"), clear(FREE_ALL), exit(exitcode));
+    if (getcore()->main_pid == getpid())
+        printf("exit\n");
+    (clear(FREE_ALL), exit(exitcode));
 }
