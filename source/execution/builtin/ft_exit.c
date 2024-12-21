09007b59e80c8edc17418ed0c8d71785f5bad742
ayoubelmohamedi <ayoubprog61@gmail.com>
2024-12-21 13:47:50 +0100

    fix: improve exit code handling in ft_exit function

--- source/execution/builtin/ft_exit.c
+++ source/execution/builtin/ft_exit.c
@@ -4,26 +4,22 @@
 int    ft_exit()
 {
     char **cmd;
-    int exitcode;
     int i;
 
     cmd = getcore()->cmd->cmd;
     i = -1;
-    exitcode = getcore()->exit_code;
     if (cmd[1] && cmd[2])
         return (pexit(": exit: too many arguments", 2, EXIT), 2);
     if (cmd[1])
     {
+        if (cmd[1][0] == '-')
+            ++i;
         while (cmd[1][++i])
         {
             if (!ft_isdigit((int)cmd[1][i]))
-                return (pexit(ft_strjoin(ft_strjoin(": exit: ", cmd[1]), ": argument must be a number"), 2, 0), 2);
-            if (i >= 4)
-                return(pexit(EXIT_USAGE, 2, 0), 2);
+                return (pexit(ft_strjoin(ft_strjoin(": exit: ", cmd[1]), ": argument must be a number"), 2, EXIT), 2); 
         }
-        exitcode = ft_atoi(cmd[1]);
-        if (exitcode > 255)
-            return(pexit(EXIT_USAGE, 2, 0), 2);
+        getcore()->exit_code = ft_atoi(cmd[1]);
     }
-    (clear(FREE_ALL), exit(exitcode));
+    (clear(FREE_ALL), exit(getcore()->exit_code));
 }
